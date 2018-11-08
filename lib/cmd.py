d0dcd4cbc48bf9daca135d6f593f6635b8484928
ekultek <Ekultek@users.noreply.github.com>
2018-11-08 10:24:43 -0600

    edited some stuff on the parser to make it display nicer

--- lib/cmd.py
+++ lib/cmd.py
@@ -36,31 +36,35 @@ class OptParser(argparse.ArgumentParser):
         parser = argparse.ArgumentParser()
         parser.add_argument(
             "-u", "--url", dest="urlToUse", metavar="http://test.com/test.php?id=",
-            help="pass a URL to test for XSS vulnerabilities"
+            help="pass a URL to test for XSS vulnerabilities. it is recommended that you use a URL with a query "
+                 "parameter"
         )
         parser.add_argument(
             "-a", "--amount", type=int, dest="verificationAmount", metavar="VERIFY",
-            help="how many verifications steps to be taken"
+            help="how many verifications steps to be taken, this will determine how reliable the payload is. the more "
+                 "verification steps the more reliable the payload will be (*default=5)"
         )
         parser.add_argument(
             "-f", "--find", type=int, dest="amountToFind", metavar="AMOUNT",
-            help="find this amount of working payloads"
+            help="attempt to find this amount of working payloads, specifying this does not guarantee you will find "
+                 "this amount of working payloads (*default=25)"
         )
         parser.add_argument(
             "-t", "--time", type=int, dest="testTime", metavar="TIME",
-            help="amount of time in seconds to spend on testing"
+            help="amount of time in seconds to spend on testing, this will be used as a timer for the verification"
+                 " (*default=35s)"
         )
         parser.add_argument(
-            "-p", "--payloads", nargs="+", dest="providedPayloads", metavar="SCRIPT,SCRIPT,SCRIPT",
-            help="pass a comma separated list of your own payloads"
+            "-p", "--payloads", nargs="+", dest="providedPayloads", metavar=("SCRIPT,", "SCRIPT,"),
+            help="pass a comma separated list of your own payloads, must contain at least 5 payloads"
         )
         parser.add_argument(
             "-F", "--file", dest="payloadFile", metavar="FILE-PATH",
-            help="pass a file containing payloads"
+            help="pass a textual file containing payloads one per line, must contain at least 5 payloads"
         )
         parser.add_argument(
             "-v", "--verbose", action="store_true", dest="runVerbose",
-            help="run in verbose mode"
+            help="run in verbose mode and display more output (*default=False)"
         )
         parser.add_argument(
             "--proxy", dest="proxyToUse", metavar="TYPE://IP:PORT",
@@ -68,6 +72,10 @@ class OptParser(argparse.ArgumentParser):
         )
         parser.add_argument(
             "-H", "--headers", dest="extraHeaders", action=StoreDictKeyPairs, metavar="HEADER=VALUE,HEADER:VALUE",
-            help="Add your own custom headers to the request"
+            help="add your own custom headers to the request (*default=connection,user-agent)"
+        )
+        parser.add_argument(
+            "--throttle", type=int, dest="throttleTime", metavar="TIME (secs)", default=0,
+            help="throttle each request with a sleep time (*default=0)"
         )
         return parser.parse_args()
