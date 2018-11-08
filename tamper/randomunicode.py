fb87ff7829c5be7a1f335e830c8c4cb528f5055b
ekultek <Ekultek@users.noreply.github.com>
2018-11-08 11:57:56 -0600

    fixes random unicode generation

--- tamper/randomunicode.py
+++ tamper/randomunicode.py
@@ -7,12 +7,11 @@ __PRIORITY__ = 10
 
 def tamper(script):
     acceptable_characters = string.hexdigits
-    lowercase = string.ascii_lowercase
     retval = []
     for c in script:
         do_it = random.randint(0, len(script)) in range(len(retval) - 2, len(retval))
         if do_it:
             if c in acceptable_characters:
-                c = "{}\u00{}{}".format(c, random.randint(1, 9), random.choice(lowercase))
+                c = u"\\u%04x" % random.randrange(0x10000)
         retval.append(c)
     return ''.join(retval)
