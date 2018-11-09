41847b3c04c0d2b48aa5cf956cf1eded9cac58db
ekultek <Ekultek@users.noreply.github.com>
2018-11-09 14:25:55 -0600

    added a test for sanitization of scripts

--- lib/requester.py
+++ lib/requester.py
@@ -13,24 +13,25 @@ class Requester(object):
     def __init__(self, url, script, headers=None, proxy=None, **kwargs):
         self.url = url
         self.script = script
+        self.original_url = url
         self.headers = headers if headers is not None else lib.settings.HEADERS
         self.proxy = {"http": proxy, "https": proxy} if proxy is not None else {}
         self.throttle = kwargs.get("throttle", 0)
         self.timeout = kwargs.get("timeout", 7)
 
-    def load_url(self, marker=False):
+    def load_url(self, prefix, suffix, marker=False):
         if not marker:
-            return self.url + self.script
+            return self.url + prefix + self.script + suffix
         else:
             marker_index = self.url.index("*")
-            self.url = self.url[:marker_index] + self.script + self.url[marker_index:]
+            self.url = self.url[:marker_index] + prefix + self.script + suffix + self.url[marker_index:]
             return self.url.replace("*", "")
 
-    def make_request(self, marker=False):
+    def make_request(self, marker=False, prefix="", suffix=""):
         retval = {}
         try:
             time.sleep(self.throttle)
-            self.url = self.load_url(marker=marker)
+            self.url = self.load_url(marker=marker, prefix=prefix, suffix=suffix)
             req = requests.get(self.url, timeout=self.timeout, headers=self.headers, proxies=self.proxy)
             content = req.content
             retval[self.script] = content
@@ -79,3 +80,24 @@ class Requester(object):
                         except ValueError:
                             pass
         return retval
+
+    def check_for_sanitize(self):
+        script = '<`"">'
+        regex_script = ""
+        change_dict = {"<": "&lt;", '"': "&quot;", ">": "&gt;"}
+        for c in script:
+            if c in change_dict.keys():
+                c = change_dict[c]
+            regex_script += c
+        regex_script = re.compile(regex_script)
+        try:
+            url = self.original_url + script
+            req = requests.get(url, timeout=self.timeout, headers=self.headers, proxies=self.proxy)
+            content = req.content
+            if regex_script.search(content) is not None:
+                return True
+            else:
+                return False
+        except Exception:
+            return None
+
